b58c8c90a92d2e5a7af3fe931b1edb88faee83c5
ForbesLindesay <forbes@lindesay.co.uk>
2013-05-26 20:10:29 +0100

    Run in jit.su

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 var github = require('./lib/github')
 
 github.watchers('visionmedia', 'jade')
-  .pipe(github.processUsers('boof', function (err) {
+  .pipe(github.processUsers('bbnnt', function (err) {
     if (err) console.log(err.stack || err.message || err)
     else console.log('done')
   }))
 
-require('http').createServer(function (req, res) {
-  if (req.url != '/log') return
-  console.log = function (str) {
-    try {
-      res.write(str)
-    } catch (ex) { }
+var listeners = []
+var log = console.log
+console.log = function (msg) {
+  for (var i = 0; i < listeners.length; i++) {
+    listeners[i](msg)
   }
+  return log.apply(console, arguments)
+}
+require('http').createServer(function (req, res) {
+  if (req.url != '/log') {
+    res.writeHead(404)
+    return res.end('404')
+  }
+  res.setHeader('Content-Type', 'text/plain')
+  listeners.push(handler)
+  function handler(msg) {
+    res.write(msg + '\n')
+  }
+  setTimeout(function () {
+    listeners = listeners.filter(function (h) { return h != handler})
+    res.end()
+  }, 10000)
 }).listen(3000)
