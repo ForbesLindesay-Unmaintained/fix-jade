1f101c6553ea7563bb7ad61048ebec6cb57cc658
ForbesLindesay <forbes@lindesay.co.uk>
2013-06-08 00:08:18 +0100

    Remove bot stuff

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 
+var relative = require('path').relative
+
 var color = require('bash-color')
 var purple = color.purple
 var green = color.green
 
-var fix = require('./index.js')
+var fix = require('../index.js')
 
-fix.processDir(process.cwd(), function (path, replacements) {
+fix.processDir(process.cwd(), function (path, replacementsCount) {
   var name = relative(process.cwd(), path).replace(/\\/g, '/')
-  if (replacements) {
-    console.log(purple('fixed') + ' "' + name + '" ' + purple('by making ' + replacements(replacements)))
+  if (replacementsCount) {
+    console.log(purple('fixed') + ' "' + name + '" ' + purple('by making ' + replacements(replacementsCount)))
   } else {
     console.log(green('skipped') + ' "' + name + '" ' + green('as it was already correct.'))
   }
